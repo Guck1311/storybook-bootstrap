a951582081beb811994980c42e10d5fca667b89d
Marcel Burgstaller <marcel.burgstaller@jku.at>
2025-08-06 10:54:47 +0200

    chore: cypress example

--- libs/shared/ui-components/src/lib/ui-components/ui-components.cy.ts
+++ libs/shared/ui-components/src/lib/ui-components/ui-components.cy.ts
@@ -6,5 +6,6 @@ describe('UiComponents', () => {
   it('should mount and display content', () => {
     mount(UiComponents);
     cy.contains('UiComponents works!').should('exist');
+    cy.get('.btn-primary').should('have.css', 'border-radius', '0.375rem');
   });
 });
